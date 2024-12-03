7d85b02b966b2815737d00a63f7c131bae952aa1
Alexander Yao <9095664+TheAlexYao@users.noreply.github.com>
2024-12-03 20:04:31 +0700

    wallet

--- src/components/providers/WagmiProvider.tsx
+++ src/components/providers/WagmiProvider.tsx
@@ -2,13 +2,18 @@ import { createConfig, http, WagmiProvider } from "wagmi";
 import { base } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { frameConnector } from "~/lib/connector";
+import { coinbaseWallet, injected } from "wagmi/connectors";
 
 export const config = createConfig({
   chains: [base],
   transports: {
     [base.id]: http(),
   },
-  connectors: [frameConnector()],
+  connectors: [
+    injected(),
+    coinbaseWallet({ appName: "Meme vs Meme" }),
+    frameConnector(),
+  ],
 });
 
 const queryClient = new QueryClient();
